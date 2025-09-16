86a35e2883a3a3a38c57b96c88a09d1c44f77efc
Yashi Garg <yashigarg016@gmail.com>
2025-09-16 23:56:47 +0530

    edited search.py

--- app/routes/search.py
+++ app/routes/search.py
@@ -38,8 +38,18 @@
 #         "duckduckgo": duck_results
 #     }
 
+# from fastapi import APIRouter, Query
+# from app.services.search_providers import perform_search
+
+# router = APIRouter()
+
+# @router.get("/")
+# async def search(query: str = Query(...)):
+#     results = await perform_search(query)
+#     return {"results": results}
+
 from fastapi import APIRouter, Query
-from app.services.search_providers import perform_search
+from services.search_providers import perform_search
 
 router = APIRouter()
 
